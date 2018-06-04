9756920a216f0ee50ed6b815400c0bfc11a8f3f1
WayneAlbuquerque <waynealbuquerque17@gmaail.com>
2018-06-04 14:31:42 -0300

    Retirando comentarios n usados

--- ControleDeEntrada/ControleDeEntrada/main.c
+++ ControleDeEntrada/ControleDeEntrada/main.c
@@ -16,12 +16,10 @@
 #define clr_bit(adress,bit) (adress&=~(1<<bit))
 #define rd_bit(adress,bit) (adress&(1<<bit))
 #define cpl_bit(adress,bit) (adress^=(1<<bit))
-//#define USART_BAUDRATE 9600
-//#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) -1)
-#define LCD_Dir  DDRB			/* Define LCD data port direction */
-#define LCD_Port PORTB			/* Define LCD data port */
-#define RS PB0				/* Define Register Select pin */
-#define EN PB1 				/* Define Enable signal pin */
+#define LCD_Dir  DDRB			
+#define LCD_Port PORTB			
+#define RS PB0			
+#define EN PB1 			
 
 enum Estados {Aguarda, Liberacao, Configuracao, Cadastro, Deleta, LogDeEntradas}; //Estados do sistema
 char teclado[4][3] = {{'3','2','1'},{'6','5','4'},{'9','8','7'},{35,'0',42}}; //valores do teclado matricial 4x3 espelhados devido a montagem eletronica feita
@@ -34,13 +32,13 @@ char buffer[10] = "",ConfirmaSenha[10]="",DeletID[5]=""; // Buffer para captura 
 int liberacao = 0, Est = 0, FP=0, FC=0, FS=0, indexDeletID=-1,FD = 0,indexTela=0,cont = 0;
 
 void LCD_Command(unsigned char cmnd){
-	LCD_Port = (LCD_Port & 0x0F) | (cmnd & 0xF0); /* sending upper nibble */
-	LCD_Port &= ~ (1<<RS);		/* RS=0, command reg. */
-	LCD_Port |= (1<<EN);		/* Enable pulse */
+	LCD_Port = (LCD_Port & 0x0F) | (cmnd & 0xF0); 
+	LCD_Port &= ~ (1<<RS);		
+	LCD_Port |= (1<<EN);		
 	_delay_us(1);
 	LCD_Port &= ~ (1<<EN);
 	_delay_us(200);
-	LCD_Port = (LCD_Port & 0x0F) | (cmnd << 4);  /* sending lower nibble */
+	LCD_Port = (LCD_Port & 0x0F) | (cmnd << 4);  
 	LCD_Port |= (1<<EN);
 	_delay_us(1);
 	LCD_Port &= ~ (1<<EN);
@@ -50,36 +48,36 @@ void LCD_Command(unsigned char cmnd){
 
 void LCD_Char( unsigned char data )
 {
-	LCD_Port = (LCD_Port & 0x0F) | (data & 0xF0); /* sending upper nibble */
-	LCD_Port |= (1<<RS);		/* RS=1, data reg. */
+	LCD_Port = (LCD_Port & 0x0F) | (data & 0xF0); 
+	LCD_Port |= (1<<RS);		
 	LCD_Port|= (1<<EN);
 	_delay_us(1);
 	LCD_Port &= ~ (1<<EN);
 	_delay_us(200);
-	LCD_Port = (LCD_Port & 0x0F) | (data << 4); /* sending lower nibble */
+	LCD_Port = (LCD_Port & 0x0F) | (data << 4); 
 	LCD_Port |= (1<<EN);
 	_delay_us(1);
 	LCD_Port &= ~ (1<<EN);
 	_delay_ms(2);
 }
 
-void LCD_Init (void)			/* LCD Initialize function */
+void LCD_Init (void)			
 {
-	LCD_Dir = 0xFF;			/* Make LCD port direction as o/p */
-	_delay_ms(20);			/* LCD Power ON delay always >15ms */	
-	LCD_Command(0x02);		/* send for 4 bit initialization of LCD  */
-	LCD_Command(0x28);              /* 2 line, 5*7 matrix in 4-bit mode */
-	LCD_Command(0x0c);              /* Display on cursor off*/
-	LCD_Command(0x06);              /* Increment cursor (shift cursor to right)*/
-	LCD_Command(0x01);              /* Clear display screen*/
+	LCD_Dir = 0xFF;			
+	_delay_ms(20);				
+	LCD_Command(0x02);		
+	LCD_Command(0x28);              
+	LCD_Command(0x0c);             
+	LCD_Command(0x06);              
+	LCD_Command(0x01);              
 	_delay_ms(2);
 }
 
 
-void LCD_String (char *str)		/* Send string to LCD function */
+void LCD_String (char *str)		
 {
 	int i;
-	for(i=0;str[i]!=0;i++)		/* Send each char of string till the NULL */
+	for(i=0;str[i]!=0;i++)		
 	{
 		LCD_Char (str[i]);
 	}
@@ -88,20 +86,7 @@ void LCD_String (char *str)		/* Send string to LCD function */
 void LCD_Clear(){
 	LCD_Command (0x01);		/* Clear display */
 	_delay_ms(2);
-	LCD_Command (0x80);		/* Cursor at home position */
-}
-
-void USART_Transmit( unsigned char data ) // transmite um caracter para o serial do Atmega328p
-{
-	while ( !(UCSR0A & (1<<UDRE0)));
-	UDR0 = data;	
-}
-
-void print(char dados[100]){ // Funcao que escreve uma string no terminal de ate 100 caracteres
-	int i = 0;
-	while(i != strlen(dados)){
-		USART_Transmit(dados[i++]);
-	}
+	LCD_Command (0x80);		/* Cursor pro inicio */
 }
 
 void print_LCD(char L1[16],char L2[16]){
@@ -129,7 +114,6 @@ int TestaSenha(char senha[10]){ //Funcao para comparar a senha digitada com o ba
 					}
 				}
 				FS = 1;
-				//print("Senha incorreta1");
 				break;
 			case 1:
 				if(strcmp(senha,"171294") == 0){ // Testa se e a senha mestre
@@ -141,15 +125,13 @@ int TestaSenha(char senha[10]){ //Funcao para comparar a senha digitada com o ba
 					return 2;
 				}
 				FS = 2;
-				//print("Senha incorreta2");
 				break;
 			case 2:
-				//print("Senha incorreta");
 				strcpy(buffer,""); // Limpa o buffer
-				//if(Est!=0){
+
 				FP=0;
 				FS = 0;
-				//}
+
 				return 0;
 				break;
 		}
@@ -158,27 +140,18 @@ int TestaSenha(char senha[10]){ //Funcao para comparar a senha digitada com o ba
 }
 
 void Mensagem(){ // Escreve mensagens
-	//char msg[100];
+
 	if(Est==0){
-		//strcpy(msg,"\r\n     BEM VINDO \r\n ESTE E O LAB DGT2 \r\n DIGITE SUA SENHA! \r\n");
 		indexTela = 0;
 	}else if(Est == 2){
-		//strcpy(msg,"\r\n[1] - Cadastra senha \r\n[2] - Deleta Senha \r\n[3] - Log de Entradas \r\n[4] - Sair \r\n");
 		indexTela=1;
 	}else if(Est == 3){
-		//sprintf(msg,"Cadastro, seu ID e %s \r\nNova senha(ex 3578951#): ",userIDs[nsenhas]);
 		indexTela = 6;
 	}else if(Est == 4){
-		//strcpy(msg,"\r\nInforme o ID que perdera o acesso:\r\n");
 		indexTela = 6;
 	}else if(Est == 5){
-		//strcpy(msg,"\r\n[3] - Log de Entradas\r\n");
 		indexTela = 6;
 	}
-	//int i = 0;
-	//while(i != strlen(msg)){
-	//	USART_Transmit(msg[i++]);
-	//}
 	FP = 1;	
 }
 
@@ -210,31 +183,21 @@ int cadastro(char senha[10]){
 		case 0:
 			if(strlen(senha)!=7){
 				print_LCD("senha invalida","7 digitos");
+				_delay_ms(1000);
 				FP = 0;
 				strcpy(buffer,"");
 				}else{
 					strcpy(ConfirmaSenha,buffer);
 					strcpy(buffer,"");	
-					//strcpy(Tela[6],"");				
-					//print("\r\nConfirma senha");
-					//print(ConfirmaSenha);
-					//print("? \r\n[1]- sim, [2] - nao\r\n");
-					//strcat(Tela[6],"Confirma ");
-					//strcat(Tela[6],ConfirmaSenha);
-					//strcat(Tela[6],"?");
-					//sprintf(Tela[6],"Confirma %s?",ConfirmaSenha);
 					print_LCD("Confirmar?","[1]-sim,[2]-nao");
 
 					FC = 1;
 				}
 			break;
-		case 1:
-			//sprintf(Tela[6],"Confirma %s?",ConfirmaSenha);
-			
+		case 1:			
 			if(buffer[0] == '1'){
 				ConfirmaSenha[strlen(ConfirmaSenha)-1] = '\0';
 				strcpy(senhas[nsenhas],ConfirmaSenha);
-				//print("\r\nSenha cadastrada com sucesso");
 				print_LCD("Senha cadastrada","");
 				nsenhas++;
 				strcpy(buffer,"");
@@ -258,7 +221,6 @@ void Sort(){
 	char senhasTmp[16][7];
 	char userIDsTmp[16][5];
 	int j = 0;
-	//char info[20];
 	for (int i = 0; i<15; i++){
 		if(i!=indexDeletID){
 			strcpy(senhasTmp[i],senhas[j]);
@@ -277,8 +239,6 @@ void Sort(){
 	for(int i = 0;i<16;i++){
 		strcpy(senhas[i],senhasTmp[i]);
 		strcpy(userIDs[i],userIDsTmp[i]);
-		//sprintf(info,"ID: %s - Senha: %s\r\n",userIDs[i],senhas[i]);
-		//print(info);
 	}
 	nsenhas--;
 }
@@ -289,14 +249,11 @@ int Deletar(char ID[10]){
 	switch(FD){
 		case 0:
 		if(strlen(ID)!=5){
-			//print("o ID deve ter 5 digitos terminando com #");
 			strcpy(buffer,"");
 		}else{
 			for(int c = 0; c < 16; c++){
 				if(strcmp(DeletID,userIDs[c]) == 0){	// Testa se a senha digitada e uma do banco de senhas
-					//strcpy(buffer,""); // Limpa o buffer
 					indexDeletID = c;
-					//FS = 0;
 				}
 			}
 		}
@@ -309,11 +266,8 @@ int Deletar(char ID[10]){
 			FP=0;
 			return 2;
 		}else{
-			//strcpy(ConfirmaSenha,buffer);
+
 			strcpy(buffer,"");
-			//print("Confirma ID ");
-			//print(DeletID);
-			//print("? \r\n[1]- sim, [2] - nao\r\n");
 			sprintf(Tela[6],"Confirma %s?",DeletID);
 			print_LCD(Tela[6],"[1]-sim,[2]-nao");
 			FD = 1;	
@@ -321,10 +275,6 @@ int Deletar(char ID[10]){
 		break;
 		case 1:
 		if(buffer[0] == '1'){
-			/*ConfirmaSenha[strlen(ConfirmaSenha)-1] = '\0';
-			strcpy(senhas[nsenhas],ConfirmaSenha);
-			print("\r\nSenha cadastrada com sucesso");
-			nsenhas++;*/
 			Sort();
 			strcpy(buffer,"");
 			strcpy(DeletID,"");
@@ -377,16 +327,8 @@ int main(void)
 	PCMSK1 |= (1<<PCINT9);
 	PCMSK1 |= (1<<PCINT10);
 
-/*
-	UCSR0B = (1 << RXEN0) | (1 << TXEN0); 
-	UCSR0C = (1 << USBS0) | (1 << UCSZ00) | (1 << UCSZ01);
-	
-	UBRR0H = (BAUD_PRESCALE >> 8);
-	UBRR0L = BAUD_PRESCALE;
-	
-	UCSR0B |= (1 << RXCIE0);
-*/
-	LCD_Init();			/* Initialization of LCD*/
+
+	LCD_Init();			/* Inicia o LCD*/
 	
 	sei();
 	Mensagem();	
@@ -418,7 +360,6 @@ int main(void)
 						indexTela++;
 					}
 					if(strlen(buffer)>0){
-						//print(buffer);
 						Est = Master(buffer); // Retorna ao esta Aguarda
 					}
 				break;
@@ -441,7 +382,6 @@ int main(void)
 				}
 				if(buffer[strlen(buffer)-1]==35||FD == 1){
 					Est = Deletar(buffer);
-					//FP = 0;
 				}
 				break;
 			case LogDeEntradas:
@@ -449,16 +389,6 @@ int main(void)
 					Mensagem();
 					print_LCD("Log Entradas","Precione *");
 				}
-				//for(int x=0;x<16;x++){
-					//char LOG[40];
-					//sprintf(Tela[6],"ID: %s - Senha: %s \r\n",userIDs[x], senhas[x]);
-					//print("ID: ");
-					//print(userIDs[x]);
-					//print(" - Senha: ");
-					//print(senhas[x]);
-					//print("\r\n");
-					//_delay_ms(10);
-				//}
 				if(buffer[strlen(buffer)-1]==35){
 					strcpy(buffer,"");
 					cont = 0;
@@ -471,24 +401,6 @@ int main(void)
 	}
 
 }
-/*
-ISR(USART_RX_vect){ // rotina quando ocorre a interrupcao na serial
-	char ReceivedByte = UDR0;
-	char str[3];
-	if(ReceivedByte){ // Testa se a variavel tem um valor se tem entra na condicao
-		if(ReceivedByte == 13){
-			str[0] = 35;          //Alimenta String com o sinal de fim
-			str[1] = '\0';		   //com o caracter lido
-			print(str);	// escreve na serial o valor da string
-			strcat(buffer,str);    // concatena str no buffer
-		}else{
-			str[0] = ReceivedByte; //Alimenta a String com a letura da serial
-			str[1] = '\0';		   //com o caracter lido
-			print(str);	// escreve na serial o valor da string
-			strcat(buffer,str);    // concatena str no buffer
-		}	
-	}		
-}*/
 
 ISR(PCINT1_vect){ // rotina quando corre interrupcao pelo teclado
 	char str[3];
@@ -496,10 +408,6 @@ ISR(PCINT1_vect){ // rotina quando corre interrupcao pelo teclado
 		p2 = 0;
 		str[0] = teclado[p1][p2]; // salva o valor da tecla na string auxiliar
 		str[1] = '\0';
-		//print(str);	// escreve na serial o valor da string
-		//if(teclado[p1][p2]==35){
-			//print(" \r\n");
-		//}
 		strcat(buffer,str);
 		print_LCD(Tela[indexTela],buffer);
 		while(rd_bit(PINC,PINC0)); // para o fluxo do codigo ate que o botao seja liberado
@@ -509,10 +417,6 @@ ISR(PCINT1_vect){ // rotina quando corre interrupcao pelo teclado
 		p2 = 1;
 		str[0] = teclado[p1][p2]; // salva o valor da tecla na string auxiliar
 		str[1] = '\0';
-		//print(str); // escreve na serial o valor da string
-		//if(teclado[p1][p2]==35){
-		//	print(" \r\n");
-		//}
 		strcat(buffer,str);
 		print_LCD(Tela[indexTela],buffer);
 		while(rd_bit(PINC,PINC1)); // para o fluxo do codigo ate que o botao seja liberado
@@ -540,7 +444,6 @@ ISR(PCINT1_vect){ // rotina quando corre interrupcao pelo teclado
 			print_LCD(User,Senha);
 			cont++;
 		}else{
-			//print(str); // escreve na serial o valor da string
 			strcat(buffer,str);
 			print_LCD(Tela[indexTela],buffer);
 		}
